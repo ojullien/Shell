11f10ca18ba1662391a4cf7c03b0a47843a7ee54
ojullien <ojullien@users.noreply.github.com>
2019-02-12 17:16:59 +0100

    ManageServices app refactoring
    Signed-off-by: ojullien <ojullien@users.noreply.github.com>

--- src/app/manageservices/app.sh
+++ src/app/manageservices/app.sh
@@ -11,17 +11,3 @@ ManageServices::help() {
     String::notice "\tDisable, start or stop a list of services defined in the config.sh file"
 }
 
-## -----------------------------------------------------------------------------
-## Trace
-## -----------------------------------------------------------------------------
-ManageServices::trace() {
-    String::separateLine
-    String::notice "App configuration: ManageServices"
-    String::notice "\tServices to disable:"
-    String::notice  "\t\t${m_SERVICES_DISABLE}"
-    String::notice "\tServices to stop:"
-    String::notice  "\t\t${m_SERVICES_STOP}"
-    String::notice "\tServices to start:"
-    String::notice  "\t\t${m_SERVICES_START}"
-    return 0
-}
