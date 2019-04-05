a6c41b9e264540069b2bfb67cdb5a05faea1a0f8
ojullien <ojullien@users.noreply.github.com>
2019-04-05 21:55:24 +0200

    SSH
    Signed-off-by: ojullien <ojullien@users.noreply.github.com>

---
+++ src/sys/ssh.sh
@@ -0,0 +1,7 @@
+## -----------------------------------------------------------------------------
+## Linux Scripts.
+## SSH functions
+##
+## @package ojullien\Shell\sys
+## @license MIT <https://github.com/ojullien/Shell/blob/master/LICENSE>
+## -----------------------------------------------------------------------------
