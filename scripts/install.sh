7b881e2f15ff94b040353bb010bb31f1d7eed627
ojullien <ojullien@users.noreply.github.com>
2019-08-06 21:55:35 +0200

    v3.2.0

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,76 +14,20 @@ set -o pipefail
 ## Current project directory, eg: /opt/Shell/scripts/
 ## -----------------------------------------------------------------------------
 readonly m_DIR_REALPATH="$(realpath "$(dirname "$0")")"
-# shellcheck source=/dev/null
-. "${m_DIR_REALPATH}/config.sh"
-# shellcheck source=/dev/null
-. "${m_DIR_REALPATH}/includes.sh"
 
 ## -----------------------------------------------------------------------------
-## Includes sources & configuration
+## Defines main directories
 ## -----------------------------------------------------------------------------
-# shellcheck source=/dev/null
-. "${m_DIR_SYS}/runasroot.sh"
-# shellcheck source=/dev/null
-. "${m_DIR_SYS}/string.sh"
-# shellcheck source=/dev/null
-. "${m_DIR_SYS}/filesystem.sh"
-# shellcheck source=/dev/null
-. "${m_DIR_SYS}/option.sh"
-# shellcheck source=/dev/null
-. "${m_DIR_SYS}/config.sh"
+readonly m_SHELL_INSTALL_SOURCE_DIR="$(realpath "${m_DIR_REALPATH}/../src")"
+readonly -a m_SHELL_PACKAGES=("sys" "autosave" "autosavedb" "clean" "createdomain" "fillwithzeros" "install" "manageservices" "pki" "savedb" "savesite" "savesystemconf" "tartime" "updatesystem");
 
 ## -----------------------------------------------------------------------------
-## Help
+## Install
 ## -----------------------------------------------------------------------------
-((m_OPTION_SHOWHELP)) && Option::showHelp && exit 0
-
-## -----------------------------------------------------------------------------
-## bash-sys must exists
-## -----------------------------------------------------------------------------
-if [[ ! -d "${m_DIR_SYS}" ]]; then
-    String::error "bash-sys is not installed on ${m_DIR_SYS}"
-    exit 1
-fi
-
-## -----------------------------------------------------------------------------
-## Start
-## -----------------------------------------------------------------------------
-String::separateLine
-String::notice "Today is: $(date -R)"
-String::notice "The PID for $(basename "$0") process is: $$"
-Console::waitUser
-
-## -----------------------------------------------------------------------------
-## Parse the app options and arguments
-## -----------------------------------------------------------------------------
-declare -i iReturn=1
-
-while (( "$#" )); do
-    case "$1" in
-    -t|--trace)
-        shift
-        String::separateLine
-        Install::trace
-        ;;
-    -r|--remove)
-        shift
-        m_INSTALL_OPTION_REMOVE=1
-        ;;
-    *) # unknown option
-        shift
-        String::separateLine
-        Option::showHelp
-        exit 0
-        ;;
-    esac
+for m_Index in "${!m_SHELL_PACKAGES[@]}"
+do
+    m_Package="${m_SHELL_PACKAGES[$m_Index]}"
+    echo "Installing ${m_Package} ..."
+    cd "${m_DIR_REALPATH}/../src/${m_Package}/scripts" || exit 1
+    bash install.sh "$@"
 done
-
-Install::run ${m_INSTALL_OPTION_REMOVE}
-Console::waitUser
-
-## -----------------------------------------------------------------------------
-## END
-## -----------------------------------------------------------------------------
-String::notice "Now is: $(date -R)"
-exit ${iReturn}
